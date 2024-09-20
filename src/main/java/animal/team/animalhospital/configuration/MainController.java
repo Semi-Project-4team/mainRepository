91ec46df8b4c8f61c15f99a83b0ca4b61844b0af
jieunjieune <jieune120@gmail.com>
2024-09-20 17:13:50 +0900

    # style : main디자인 # 김지은
    ################
    # - 파일추가, 패키지구조수정, 메인.html제작 중
    
    ################
    # 우리힘내자

---
+++ src/main/java/animal/team/animalhospital/configuration/MainController.java
@@ -0,0 +1,13 @@
+package animal.team.animalhospital.configuration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = {"/", "/main"})
+    public String Main() {
+        return "/common/main";
+    }
+}
