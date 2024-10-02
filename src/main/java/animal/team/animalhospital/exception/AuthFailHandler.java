5bdfb330191affa8ec4bb1e373d2fa165c4b07c7
crushed-taro <97sinsang@gmail.com>
2024-10-02 17:33:49 +0900

    # feat : 로그인창 빈 란 확인 # 신성균
    ################
    # - 로그인창 빈 란 확인해서 로그인 방지 기능 추가.
    
    ################
    #

--- src/main/java/animal/team/animalhospital/exception/AuthFailHandler.java
+++ src/main/java/animal/team/animalhospital/exception/AuthFailHandler.java
@@ -4,13 +4,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
+import javax.security.auth.login.AccountLockedException;
 import java.io.IOException;
 import java.net.URLEncoder;
 
@@ -24,20 +23,16 @@ public class AuthFailHandler extends SimpleUrlAuthenticationFailureHandler {
         String errorMessage = null;
 
         if (exception instanceof BadCredentialsException) {
-
-//            errorMessage = "[Auth-Failed] 아이디가 존재하지 않거나 비밀번호가 일치하지 않습니다.";
-            errorMessage = "[Auth-Failed] 정지된 계정입니다. 문의바랍니다.";
+            errorMessage = "[Auth-Failed] 아이디가 존재하지 않거나 비밀번호가 일치하지 않습니다.";
         } else if (exception instanceof InternalAuthenticationServiceException) {
-
             errorMessage = "[Auth-Failed] 서버에서 오류가 발생되었습니다.";
         } else if (exception instanceof UsernameNotFoundException) {
-
             errorMessage = "[Auth-Failed] 존재하지 않는 이메일 입니다.";
         } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
-
             errorMessage = "[Auth-Failed] 인증 요청이 거부되었습니다.";
+        } else if (exception instanceof LockedException) {
+            errorMessage = "[Auth-Failed] 정지된 계정입니다. 문의 바랍니다.";
         } else {
-
             errorMessage = "[Auth-Failed] 알 수 없는 오류로 로그인 요청을 처리할 수 없습니다.";
         }
 
